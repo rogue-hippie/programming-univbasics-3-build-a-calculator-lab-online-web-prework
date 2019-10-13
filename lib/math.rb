2c2d244ed85782588ecd12a966f8246e62535766
funny-array-8950 <hodgkins.whitney@gmail.com>
2019-10-13 18:38:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
+
+addition(4, 5)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+
+subtraction(9, 4)
 
 def division(num1, num2)
-
+  num1 / num2
 end
+
+division(12, 3)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+
+multiplication(3, 4)
+
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+
+modulo(34, 5)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+
+square_root(73)
